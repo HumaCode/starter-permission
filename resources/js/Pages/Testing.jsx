f5bb153b6b7be0b5eae3fc1fe01b83b87035234f
Humaidi Zakaria <humaidizakaria1998@gmail.com>
2026-01-19 11:33:36 +0700

    siapkan layout

---
+++ resources/js/Pages/Testing.jsx
@@ -0,0 +1,10 @@
+import AppLayout from "@/Layouts/AppLayout";
+
+export default function Testing()
+{
+    return (
+        <AppLayout title='Testing'>
+            This is Testing Page
+        </AppLayout>
+    )
+}
